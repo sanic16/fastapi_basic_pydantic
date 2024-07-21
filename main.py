a5ebdcc39ebd9afd7f2c3a30a398fd8460bb41f3
Julio Sanic <julio.sanic.gt.256@gmail.com>
2024-07-21 13:11:27 -0600

    pydantic validations

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from typing import Optional
-from fastapi import Body, FastAPI
+from fastapi import FastAPI
 from pydantic import BaseModel, Field
 from utils import BOOKS, Book, find_book_id
 
@@ -13,6 +13,7 @@ class BookRequest(BaseModel):
     author: str = Field(min_length=3, max_length=50)
     description: str = Field(min_length=5, max_length=200)
     rating: int = Field(ge=0, le=5)
+    published_year : int = Field(gt=1999, lt=2031)
 
     model_config = {
         'json_schema_extra': {
@@ -20,7 +21,8 @@ class BookRequest(BaseModel):
                 "title": "La biblia de JavaScript",
                 "author": "Douglas Crockford",
                 "description": "Un libro para aprender JavaScript",
-                "rating": 5
+                "rating": 5,
+                "published_year": 2024
             }
         }
     }
@@ -32,9 +34,50 @@ class BookRequest(BaseModel):
 async def read_all_books():
     return BOOKS
 
+@app.get('/books/{book_id}')
+async def read_book(book_id: int):
+    for book in BOOKS:
+        if book.id == book_id:
+            return book
+    return {'message': 'Book not found'}
+
+@app.get('/books/')
+async def read_book_by_rating(book_rating: int):
+    books_to_return = []
+    for book in BOOKS:
+        if book.rating == book_rating:
+            books_to_return.append(book)
+    return books_to_return
+
+@app.get('/books/publish/')
+async def read_books_by_publish_date(published_year: int):
+    books_to_return = []
+    for book in BOOKS:
+        if book.published_year == published_year:
+            books_to_return.append(book)
+        
+    return books_to_return
+
 @app.post('/create-book')
 async def create_book(book_request: BookRequest):
    new_book = Book(**book_request.model_dump())
    new_book = find_book_id(new_book)
    BOOKS.append(new_book)
+   return new_book
 
+@app.put('/books/update_book')
+async def update_book(book: BookRequest):
+    
+    for i in range(len(BOOKS)):
+        if BOOKS[i].id == book.id:
+            BOOKS[i] = book
+            return book
+    return {'message': 'Book not found'}
+
+@app.delete('/books/{book_id}')
+async def delete_book(book_id: int):
+    for i in range(len(BOOKS)):
+        if BOOKS[i].id == book_id:
+            BOOKS.pop(i)
+            return {'message': 'Book deleted'}
+    return {'message': 'Book not found'}
--- utils.py
+++ utils.py
@@ -4,23 +4,25 @@ class Book:
     author: str
     description: str
     rating: int
+    published_year: int 
 
-    def __init__(self, id, title, author, description, rating):
+    def __init__(self, id, title, author, description, rating, published_year):
         self.id = id
         self.title = title
         self.author = author
         self.description = description
         self.rating = rating
+        self.published_year = published_year
 
 BOOKS = [
-    Book(1, 'Amor en tiempos de pandemia', 'Gabi Martínez', 'Una historia de amor en tiempos de pandemia', 5),
-    Book(2, 'El arte de la guerra', 'Sun Tzu', 'Un tratado militar escrito por Sun Tzu', 5),
-    Book(3, 'El principito', 'Antoine de Saint-Exupéry', 'Un cuento infantil con muchas enseñanzas', 5),
-    Book(4, 'La hija del puma', 'Monica Zak', 'Una historia de drama en Guatemala', 5),
-    Book(5, 'Python Crash Course', 'Eric Matthes', 'Un libro para aprender Python', 5),
-    Book(6, 'El código Da Vinci', 'Dan Brown', 'Una novela de misterio y suspenso', 3),
-    Book(7, 'El alquimista', 'Paulo Coelho', 'Una novela de aventuras y misterio', 4),
-    Book(8, 'El amor en los tiempos del cólera', 'Gabriel García Márquez', 'Una historia de amor en Colombia', 2),
+    Book(1, 'Amor en tiempos de pandemia', 'Gabi Martínez', 'Una historia de amor en tiempos de pandemia', 5, 2020),
+    Book(2, 'El arte de la guerra', 'Sun Tzu', 'Un tratado militar escrito por Sun Tzu', 5, 2021),
+    Book(3, 'El principito', 'Antoine de Saint-Exupéry', 'Un cuento infantil con muchas enseñanzas', 5, 1954),
+    Book(4, 'La hija del puma', 'Monica Zak', 'Una historia de drama en Guatemala', 5, 2004),
+    Book(5, 'Python Crash Course', 'Eric Matthes', 'Un libro para aprender Python', 5, 2015),
+    Book(6, 'El código Da Vinci', 'Dan Brown', 'Una novela de misterio y suspenso', 3, 1999),
+    Book(7, 'El alquimista', 'Paulo Coelho', 'Una novela de aventuras y misterio', 4, 1995),
+    Book(8, 'El amor en los tiempos del cólera', 'Gabriel García Márquez', 'Una historia de amor en Colombia', 2, 1985),
 ]
 
 def find_book_id(book: Book):
@@ -33,4 +35,4 @@ def find_book_id(book: Book):
 
 def find_book_index(book: Book):
     book.id = 1 if len(BOOKS) == 0 else BOOKS[-1].id + 1
-    return boo 
+    return book 
